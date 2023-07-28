077b00de35c6de1d8209fd0a0b850f12dde8ee8e
Martin6363 <delta.zelta@mail.ru>
2023-07-28 20:49:34 +0400

    TodoItem code change

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -5,12 +5,7 @@ import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 
 export function TodoItem({ todo, onChange, onDelete }) {
   const [isChecked, setIsChecked] = useState(todo.isDone);
-
-  const [isLocalChecked, setIsLocalChecked] = useState(todo.isDone);
-  if (isLocalChecked !== isChecked) {
-    setIsLocalChecked(isChecked);
-  }
-
+  
   return (
     <div className='list-text-cont'>
         <label className={`label-list${isChecked ? ' line-text' : ''}`}>
@@ -19,8 +14,7 @@ export function TodoItem({ todo, onChange, onDelete }) {
               <input type='checkbox'
                 checked={isChecked} 
                 onChange={(e)=> {
-                  setIsLocalChecked(e.target.checked);
-                  setIsChecked(e.target.checked)
+                  setIsChecked(e.target.checked);
                   onChange({
                     ...todo,
                     isDone: e.target.checked
